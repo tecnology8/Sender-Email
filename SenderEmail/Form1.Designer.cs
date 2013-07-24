8371779e750d237e316e2a682dc3f4e571000534
Ruddy Cruceta <ruddycruceta@hotmail.com>
2013-07-24 11:29:26 -0430

    Sender Email
    para el envio de correos

---
+++ SenderEmail/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SenderEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrom = new Telerik.WinControls.UI.RadTextBox();
+            this.btnInsert = new Telerik.WinControls.UI.RadButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSend = new Telerik.WinControls.UI.RadButton();
+            this.txtPassword = new Telerik.WinControls.UI.RadTextBox();
+            this.txtFor = new Telerik.WinControls.UI.RadTextBox();
+            this.txtMessage = new Telerik.WinControls.UI.RadTextBox();
+            this.txtSubject = new Telerik.WinControls.UI.RadTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnInsert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSend)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMessage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSubject)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(82, 49);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(245, 20);
+            this.txtFrom.TabIndex = 1;
+            this.txtFrom.TabStop = false;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(82, 166);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(110, 24);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.Click += new System.EventHandler(this.BtnInsertClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "For:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Subject:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(518, 465);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(110, 24);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.Click += new System.EventHandler(this.BtnSendClick);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(82, 90);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(245, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TabStop = false;
+            // 
+            // txtFor
+            // 
+            this.txtFor.Location = new System.Drawing.Point(82, 140);
+            this.txtFor.Name = "txtFor";
+            this.txtFor.Size = new System.Drawing.Size(546, 20);
+            this.txtFor.TabIndex = 3;
+            this.txtFor.TabStop = false;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.AutoSize = false;
+            this.txtMessage.Location = new System.Drawing.Point(33, 282);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(595, 177);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.TabStop = false;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(33, 226);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(595, 20);
+            this.txtSubject.TabIndex = 4;
+            this.txtSubject.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 500);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtFor);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnInsert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSend)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMessage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSubject)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Telerik.WinControls.UI.RadTextBox txtFrom;
+        private Telerik.WinControls.UI.RadButton btnInsert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Telerik.WinControls.UI.RadButton btnSend;
+        private Telerik.WinControls.UI.RadTextBox txtPassword;
+        private Telerik.WinControls.UI.RadTextBox txtFor;
+        private Telerik.WinControls.UI.RadTextBox txtMessage;
+        private Telerik.WinControls.UI.RadTextBox txtSubject;
+    }
+}
+
---
+++ SenderEmail/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace SenderEmail
+{
+    public partial class Form1 : Form
+    {
+        private bool adj = false;
+        private string File;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSendClick(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            
+            mail.From = new MailAddress(txtFrom.Text);
+
+            mail.To.Add(txtFor.Text);
+            mail.Subject = txtSubject.Text;
+            mail.Body = txtMessage.Text;
+            mail.IsBodyHtml = false;
+            mail.Priority = MailPriority.Normal;
+
+            if (adj == true)
+            {
+                Attachment attach = new Attachment(@File);
+                mail.Attachments.Add(attach);
+                adj = false;
+            }
+
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Credentials = new NetworkCredential(txtFrom.Text, txtPassword.Text);
+
+                smtp.Host = "smtp.live.com";
+                smtp.Port = 25;
+
+                //smtp.Port = 995;  pop3.live.com --- Hotmail
+                //smtp.Port = 587; pop.gmail.com --- Gmail
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+                MessageBox.Show("Mail Sent");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to send mail", ex.Message);
+            }
+                mail.Dispose();
+                Clean();
+                txtFrom.Focus();
+        }
+        
+        private void BtnInsertClick(object sender, EventArgs e)
+        {
+            adj = true;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Choose File";
+            file.InitialDirectory = @"c:\";
+            file.Filter = "All files(*.*)|*.*";
+            file.FilterIndex = 1;
+            file.RestoreDirectory = true;
+            file.ShowDialog();
+            File = file.FileName;
+        }
+
+        public void Clean()
+        {
+            txtFor.Text = string.Empty;
+            txtFrom.Text = string.Empty;
+            txtMessage.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtSubject.Text = string.Empty;
+        }
+    }
+}
